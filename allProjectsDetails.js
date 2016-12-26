14ccafb036c51dabb591849946923a95212e3447
Yuridia Larios Aispuro <yuriydani@gmail.com>
2016-12-25 22:14:42 -0800

    function for coded projects

--- allProjectsDetails.js
+++ allProjectsDetails.js
@@ -17,7 +17,7 @@ function wordpressRecords(data)
                                                 if (project_picture) {
                                                     project_info += `<div class="thumbnail">`;
 
-                                  
+
                                                 $.each(project_picture, function(i, pic){
                                                     project_info +=`<a href="projectDetail.html?projectID=${project.id}"><img src="${pic.url}"></a>`;
 
@@ -37,3 +37,42 @@ function wordpressRecords(data)
 
 
 $.get(all_project_records, wordpressRecords);
+
+
+
+// all wordpress projects
+function codedRecords(data)
+{
+    $(data.records).each(function(index, project)
+                         {
+                           var project_name = project.fields['Name']
+                           var project_picture = project.fields['Photos']
+                           var coded_check = project.fields['Coded']
+
+                           var project_info = ''
+                           if (project_name && coded_check) {
+                                            project_info += `<div class="column">`;
+                                                project_info += `<div class="col-sm-6 col-md-4">`;
+                                                if (project_picture) {
+                                                    project_info += `<div class="thumbnail">`;
+
+
+                                                $.each(project_picture, function(i, pic){
+                                                    project_info +=`<a href="projectDetail.html?projectID=${project.id}"><img src="${pic.url}"></a>`;
+
+                         });
+
+                        }
+
+                         project_info += `</div>`;
+                         project_info += `<div class="caption">${project_name} </div>`;
+                         project_info += `</div>`;
+                         project_info += `</div>`;
+
+                         }
+      $('.codedProjects').append(project_info);
+                       });
+}
+
+
+$.get(all_project_records, codedRecords);
