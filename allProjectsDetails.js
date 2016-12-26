2a821e1c67a60b33e13e18ab407953da9d58945c
Yuridia Larios Aispuro <yuriydani@gmail.com>
2016-12-25 21:08:29 -0800

    again

--- allProjectsDetails.js
+++ allProjectsDetails.js
@@ -1,6 +1,6 @@
 /*global $*/
 var api_key= 'keyx8KoDimBmlkxCL';
-var all_project_records= 'https:/q3O4DNe/Art%20Pieces?api_key=' + api_key;
+var all_project_records= 'https://api.airtable.com/v0/appUun0GAPq3O4DNe/Art%20Pieces?api_key=' + api_key;
 
 
 
